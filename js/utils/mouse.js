c3c5e77d379e2ebc0c4a24c62c770e01768c1340
beProsto <lavionperavion2@gmail.com>
2021-06-06 21:39:29 +0200

    tocuhscreen should work now.

--- js/utils/mouse.js
+++ js/utils/mouse.js
@@ -1,4 +1,4 @@
-let mouse = {position: [0.0, 0.0], velocity: [0.0, 0.0], pressedButtons: [false, false, false, false, false], wheel: 0.0};
+let mouse = {position: [0.0, 0.0], velocity: [0.0, 0.0], pressedButtons: [false, false, false, false, false], wheel: 0.0, _touchintlpos: [0.0, 0.0]};
 window.onmousemove = function(e) { 
 	mouse.position[0] = e.clientX; 
 	mouse.position[1] = e.clientY; 
@@ -12,10 +12,21 @@ window.onwheel = function(e) { mouse.wheel = e.deltaY; };
 window.ontouchmove = function(e) { 
 	mouse.position[0] = e.touches[0].clientX; 
 	mouse.position[1] = e.touches[0].clientY; 
-	mouse.velocity[0] = e.touches[0].movementX;
-	mouse.velocity[1] = e.touches[0].movementY;
+	mouse.velocity[0] = mouse.position[0] - mouse._touchintlpos[0];
+	mouse.velocity[1] = mouse.position[1] - mouse._touchintlpos[1];
+	mouse._touchintlpos[0] = mouse.position[0];
+	mouse._touchintlpos[1] = mouse.position[1];
 };
-window.ontouchstart = function(e) { window.ontouchmove(e); mouse.pressedButtons[0] = true; };
+window.ontouchstart = function(e) { 
+	mouse.position[0] = e.touches[0].clientX; 
+	mouse.position[1] = e.touches[0].clientY; 
+	mouse.velocity[0] = 0.0;
+	mouse.velocity[1] = 0.0;
+	mouse._touchintlpos[0] = mouse.position[0];
+	mouse._touchintlpos[1] = mouse.position[1];
+	
+	mouse.pressedButtons[0] = true; 
+};
 window.ontouchend = function(e) { mouse.pressedButtons[0] = false; };
 window.ontouchcancel = function(e) { mouse.pressedButtons[0] = false; };
 
