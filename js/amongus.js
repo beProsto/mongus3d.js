17797125aec16989e2a1e6e055c3e16ad99705f2
beProsto <lavionperavion2@gmail.com>
2021-06-24 15:46:54 +0200

    The button is disabled when the player presses it

--- js/amongus.js
+++ js/amongus.js
@@ -1,30 +1,35 @@
 const nickCheckUrl = "/nicknamecheck";
-let gameBegun  = false;
 
-document.getElementById("play-button").onclick = function() {
-	if(document.getElementById("nickInput").value) {
-		nick = document.getElementById("nickInput").value;
-		let xhr = new XMLHttpRequest();
-		xhr.open("PUT", nickCheckUrl); 
-		xhr.onload = function () {
-			let txt = xhr.responseText;
-			if (xhr.readyState == 4 && (xhr.status == "201" || xhr.status == "200")) {
-				if(txt == "ErrNickFound") {
-					alert("gracz z tym pseudonimem już dołączył");
-				}
-				else if(txt == "Posted") {
-					if(!gameBegun) {
-						gameBegun = true;
+const playButton = document.getElementById("play-button");
+
+playButton.onclick = function() {
+	if(!playButton.disabled) {
+		playButton.disabled = true;
+
+		if(document.getElementById("nickInput").value) {
+			nick = document.getElementById("nickInput").value;
+			let xhr = new XMLHttpRequest();
+			xhr.open("PUT", nickCheckUrl); 
+			xhr.onload = function () {
+				let txt = xhr.responseText;
+				if (xhr.readyState == 4 && (xhr.status == "201" || xhr.status == "200")) {
+					if(txt == "ErrNickFound") {
+						playButton.disabled = false;
+						alert("gracz z tym pseudonimem już dołączył");
+					}
+					else if(txt == "Posted") {
 						beginGame();
 					}
+				} else {
+					playButton.disabled = false;
+					console.error(txt);
 				}
-			} else {
-				console.error(txt);
-			}
-		};
-		xhr.send(nick);
-	}
-	else {
-		alert("Widzisz ten textbox?");
+			};
+			xhr.send(nick);
+		}
+		else {
+			playButton.disabled = false;
+			alert("Widzisz ten textbox?");
+		}
 	}
 };
