a7a55ed8d91f2965c069c03037d92fe280f370dc
beProsto <lavionperavion2@gmail.com>
2021-08-22 01:34:57 +0200

    useless changes

--- js/game/game.js
+++ js/game/game.js
@@ -1,5 +1,3 @@
-// let playerRotationY = 0.0;
-
 class Game {
 	constructor() {
 		this.renderer = new ezgfx.Renderer();
@@ -8,20 +6,21 @@ class Game {
 		this.player = new MainPlayer();
 
 		this.players = new Players();
-		// this.player.camera.rotationY = 0.0;
 
 		this.boxMesh = new ezgfx.Mesh();
 		this.boxMesh.loadFromOBJ("/obj/box.obj");
-		this.boxMat = new ezgfx.Material(null, null, "vec4 shader() { return texture(u_TexID[0], v_TexCoord); }")
+
+		this.boxMat = new ezgfx.Material(null, null, "vec4 shader() { return texture(u_TexID[0], v_TexCoord); }");
+		this.boxMat.setModel(ezgfxGlobals.identityMatrix);
+		
 		this.boxTexture = new ezgfx.Texture();
 		this.boxTexture.loadFromFile("/img/box.png");
+		
 		this.boxMat.setTexture(this.boxTexture);
-		this.boxMat.setModel(ezgfxGlobals.identityMatrix);
 	}
 
 	update(deltaTime) {
 		this.player.update(deltaTime);
-		// playerRotationY = this.player.camera.rotationY;
 
 		this.renderer.viewport(0, 0, canvas.width, canvas.height, this.player.camera);
 		this.renderer.clear([0.4, 0.3, 1.0, 1.0]);
@@ -31,12 +30,6 @@ class Game {
 		this.players.renderAllPlayersExceptTheMainPlayer(this.renderer, this.player.camera);
 	}
 }
-
-// setInterval(() => {
-// 	console.log(JSON.stringify(globals.Updates));
-// 	console.log(playerRotationY);
-// }, 300);
-
 
 // this is how looping through a list of object's values 
 // to get rid of them looks like
